e5f54e62c32b79d6448b92260c361d1b6b3e6bbe
Erik Alves <silvalveserik1@gmail.com>
2025-06-08 22:15:20 -0300

    style: :art: Improve mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu-mobile");
 const overlayMenu = document.getElementById("overlay-menu");
+const body = document.querySelector('body');
 
 let isMenuOpen = false;
 
@@ -9,10 +10,12 @@ menuBtn.addEventListener('click', () => {
         menu.classList.add('open-menu');
         overlayMenu.classList.add('open-overlay');
         menuBtn.src = '/images/icon-close.svg';
+        body.classList.add('menu-open');
     } else {
         menu.classList.remove('open-menu');
         overlayMenu.classList.remove('open-overlay');
         menuBtn.src = '/images/icon-hamburger.svg';
+        body.classList.remove('menu-open');
     }
 
     isMenuOpen = !isMenuOpen;
